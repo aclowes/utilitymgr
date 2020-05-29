954ce28c6fb54f69d1c4e28b38979e5160e8e998
Alec Clowes <aclowes@gmail.com>
2020-05-29 09:09:54 -0700

    chrome name

--- runner.sh
+++ runner.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if ! which chrome > /dev/null; then
+if ! which google-chrome > /dev/null; then
   wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
   sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
   apt-get update && apt install -y google-chrome-stable
