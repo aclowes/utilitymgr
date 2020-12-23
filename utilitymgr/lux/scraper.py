cd531d83fe9d2ae964790917bcd261d971556660
Alec Clowes <alec.clowes@cloverhealth.com>
2020-12-23 10:34:19 -0800

    refresh lux token

--- utilitymgr/lux/scraper.py
+++ utilitymgr/lux/scraper.py
@@ -1,30 +1,46 @@
 import datetime
+import json
 import os
 import pytz
 
 import requests
 
+from google.cloud import storage
+
+
 TOKEN_URL = 'https://connecteddevicesjci.b2clogin.com/te/' \
             'connecteddevicesjci.onmicrosoft.com/B2C_1A_SignIn/oauth2/v2.0/token'
-TOKEN_DATA = {
-    'refresh_token': os.environ['LUX_TOKEN'],
-    'client_id': 'b335ca43-3bde-4406-b281-8816afb7cc91',
-    'grant_type': 'refresh_token',
-}
 THERMOSTAT_URL = 'https://www.myluxstat.io/api/device/runtime?deviceid={device}&currentdate={date}'
 
 
 def main():
-    # refresh the access token
-    response = requests.post(TOKEN_URL, data=TOKEN_DATA)
-    response.raise_for_status()
-    body = response.json()
+    # download access token for cloud storage
+    client = storage.Client()
+    bucket = client.bucket('static.yawn.live')
+    blob = bucket.blob(f'utilitymgr/lux_token.json')
+    tokens = json.loads(blob.download_as_text())
 
-    # setup request headers
+    # check if the access token needs to be refreshed
     headers = {
-        'Authorization': f'Bearer {body["access_token"]}',
+        'Authorization': f'Bearer {tokens["access_token"]}',
         'Content-Type': 'application/json'
     }
+    response = requests.get('https://www.myluxstat.io/api/location/user', headers=headers)
+
+    # refresh the access token if needed
+    if response.status_code == 401:
+        request = {
+            'refresh_token': tokens['refresh_token'],
+            'client_id': tokens['client_id'],
+            'grant_type': 'refresh_token'
+        }
+        response = requests.post(TOKEN_URL, data=request)
+        response.raise_for_status()
+        refresh = response.json()
+        tokens['access_token'] = refresh['access_token']
+        tokens['refresh_token'] = refresh['refresh_token']
+        blob.upload_from_string(json.dumps(tokens))
+        headers['Authorization'] = f'Bearer {tokens["access_token"]}'
 
     # get data for each thermostat
     now = datetime.datetime.now(tz=pytz.timezone('US/Pacific')).replace(tzinfo=None).isoformat(timespec='seconds')
