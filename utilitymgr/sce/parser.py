cf935c8cc3b0f0e08a18e8366a48221c2957cdd8
Alec Clowes <aclowes@gmail.com>
2022-11-30 13:11:24 -0800

    Revert "fix kW"
    This reverts commit 6c9cb0645cd62ed63cfd008324679f1206d8ed4f.

--- utilitymgr/sce/parser.py
+++ utilitymgr/sce/parser.py
@@ -12,7 +12,7 @@ def main():
     lines = []
     for index, account in enumerate(accounts):
         df = gb.dataframe_from_xml(f'data/sce_{account}.xml')
-        line = plt.plot(df['Start Time'], df['Wh'], label=f'Account {str(account)[-2:]}')
+        line = plt.plot(df['Start Time'], df['Wh'] * 4, label=f'Account {str(account)[-2:]}')
         lines.append(line)
     plt.ylabel('Watts')
     plt.xlim(utils.week_start_end())
