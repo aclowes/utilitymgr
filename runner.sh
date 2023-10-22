1c7afd6f8ec37f7e8fde90eb9b0e9d7a0c4e21e3
Alec Clowes <aclowes@gmail.com>
2023-10-22 15:40:28 -0700

    pin chrome version

--- runner.sh
+++ runner.sh
@@ -7,13 +7,14 @@ pkill chrome || echo "none running"
 rm -rf /opt/google/chrome/chrome_debug.log
 
 if ! which google-chrome > /dev/null; then
-  wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-  sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-  apt-get update && apt install -y google-chrome-stable
+  CHROME_VERSION='114.0.5735.198-1'
+  curl -O https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}_amd64.deb
+  apt install -y ./google-chrome-stable_${CHROME_VERSION}_amd64.deb
+  rm google-chrome-stable_${CHROME_VERSION}_amd64.deb
 fi
 
-echo 'exec /usr/bin/google-chrome --no-sandbox --headless --disable-dev-shm-usage --disable-gpu --enable-crash-reporter --enable-logging --v=2 "$@"' > /usr/local/bin/google-chrome
-chmod +x /usr/local/bin/google-chrome
+echo 'exec /usr/bin/chromium --no-sandbox --headless --disable-dev-shm-usage --disable-gpu --enable-crash-reporter --enable-logging --v=2 "$@"' > /usr/local/bin/chromium
+chmod +x /usr/local/bin/chromium
 
 # clone if it doesn't exist
 ls utilitymgr || git clone https://github.com/aclowes/utilitymgr.git
@@ -25,7 +26,7 @@ pip install -q -r requirements.txt
 
 # https://googlechromelabs.github.io/chrome-for-testing/
 if ! ls chromedriver > /dev/null; then
-  curl -O https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/118.0.5993.70/linux64/chromedriver-linux64.zip
+  curl -O https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
   unzip -j chromedriver-linux64.zip
 fi
 
