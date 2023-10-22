927de9f080fdf9ce507a86c8a6afe2d39a244b0d
Alec Clowes <aclowes@gmail.com>
2023-10-22 15:52:15 -0700

    pin chrome version

--- runner.sh
+++ runner.sh
@@ -14,8 +14,8 @@ if ! which google-chrome > /dev/null; then
   rm google-chrome-stable_${CHROME_VERSION}_amd64.deb
 fi
 
-echo 'exec /usr/bin/chromium --no-sandbox --headless --disable-dev-shm-usage --disable-gpu --enable-crash-reporter --enable-logging --v=2 "$@"' > /usr/local/bin/chromium
-chmod +x /usr/local/bin/chromium
+echo 'exec /usr/bin/google-chrome --no-sandbox --headless --disable-dev-shm-usage --disable-gpu --enable-crash-reporter --enable-logging --v=2 "$@"' > /usr/local/bin/google-chrome
+chmod +x /usr/local/bin/google-chrome
 
 # clone if it doesn't exist
 ls utilitymgr || git clone https://github.com/aclowes/utilitymgr.git
@@ -28,7 +28,8 @@ pip install -q -r requirements.txt
 # https://googlechromelabs.github.io/chrome-for-testing/
 if ! ls chromedriver > /dev/null; then
   curl -O https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
-  unzip -j chromedriver_linux64.zip
+  unzip chromedriver_linux64.zip
+  rm chromedriver_linux64.zip
 fi
 
 mkdir -p data
