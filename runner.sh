464af3605ea382bbb690cac9727441887a76c47a
Alec Clowes <alec@laurel.ai>
2025-07-16 17:14:24 -0700

    fixes

--- runner.sh
+++ runner.sh
@@ -4,18 +4,18 @@ set -e
 
 # cleanup any leftover chrome instances
 pkill chrome || echo "none running"
-rm -rf /opt/google/chrome/chrome_debug.log
 
 if ! which google-chrome > /dev/null; then
-  # find them here: https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/
-  CHROME_VERSION='114.0.5735.198-1'
-  curl -O https://mirror.kraski.tv/soft/google_chrome/linux/114.0.5735.198/google-chrome-stable_${CHROME_VERSION}_amd64.deb
+  curl -O https://storage.googleapis.com/chrome-for-testing-public/138.0.7204.157/linux64/chrome-linux64.zip
+  unzip chrome-linux64.zip
   apt-get update
-  apt install -y ./google-chrome-stable_${CHROME_VERSION}_amd64.deb
-  rm google-chrome-stable_${CHROME_VERSION}_amd64.deb
+  while read -r pkg; do
+    apt-get satisfy -y --no-install-recommends "${pkg}"
+  done < chrome-linux64/deb.deps
+  rm chromedriver-linux64.zip
 fi
 
-echo 'exec /usr/bin/google-chrome --no-sandbox --headless --disable-dev-shm-usage --disable-gpu --enable-crash-reporter --enable-logging --v=2 "$@"' > /usr/local/bin/google-chrome
+echo "exec $PWD/chrome-linux64/chrome --no-sandbox --headless \"\$@\"" > /usr/local/bin/google-chrome
 chmod +x /usr/local/bin/google-chrome
 
 # clone if it doesn't exist
@@ -28,9 +28,9 @@ pip install -q -r requirements.txt
 
 # https://googlechromelabs.github.io/chrome-for-testing/
 if ! ls chromedriver > /dev/null; then
-  curl -O https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
-  unzip chromedriver_linux64.zip
-  rm chromedriver_linux64.zip
+  curl -O https://storage.googleapis.com/chrome-for-testing-public/138.0.7204.157/linux64/chromedriver-linux64.zip
+  unzip chromedriver-linux64.zip
+  rm chromedriver-linux64.zip
 fi
 
 mkdir -p data
